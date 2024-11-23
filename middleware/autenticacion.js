a11c3750d06ecf93d4758f8e458bfc593e244050
LadyFantasy <msoledadm88@gmail.com>
2024-11-23 18:35:21 +0100

    algo

--- middleware/autenticacion.js
+++ middleware/autenticacion.js
@@ -1,24 +1,6 @@
 /* const Usuarios = require("../controllers/usuariosController") */
 const Usuario = require("../models/Usuario");
 
-/* *************FUNCIÓN VIEJA SIN DESHASHEO************ */
-
-/*   Middleware para identificar qué tipo de rol tiene el usuario y redireccionarlo acorde en la función siguiente */
-/* async function autenticarUsuario(req, res, next) {
-  const { legajo, pass } = req.body;
-  const usuario = await Usuario.findOne({legajo:legajo})
-  if (usuario && usuario.contraseña == pass) {
-    req.rol = usuario.rol;
-    next();
-  } else {
-    res.send("Credenciales incorrectas");
-  }
-}
-
-module.exports = autenticarUsuario; */
-
-/* ******************************************************** */
-
 // Middleware para autenticar al usuario
 async function autenticarUsuario(req, res, next) {
   const { legajo, pass } = req.body;
--- models/usuario.js
+++ models/usuario.js
@@ -1,26 +1,24 @@
-const mongoose = require('mongoose'); 
-const bcrypt = require('bcrypt');
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const usuarioSchema = new mongoose.Schema({
-    legajo: {type: Number ,require : true},
-    contraseña: {type: String,require : true},
-    rol: {type: String, enum:["alumno/padre","profesor","administrador"] ,require : true}
+  legajo: { type: Number, require: true },
+  contraseña: { type: String, require: true },
+  rol: { type: String, enum: ["alumno/padre", "profesor", "administrador"], require: true }
 });
 
 // Hash de contraseñas antes de guardar
-usuarioSchema.pre('save', async function (next) {
-    if (!this.isModified('contraseña')) return next();
-    this.contraseña = await bcrypt.hash(this.contraseña, 10);
-    next();
-  }); 
-  
-  // Comparar contraseñas
-   usuarioSchema.methods.comparePassword = async function (contraseña) {
-    return bcrypt.compare(contraseña, this.contraseña);
-  };
-  
+usuarioSchema.pre("save", async function (next) {
+  if (!this.isModified("contraseña")) return next();
+  this.contraseña = await bcrypt.hash(this.contraseña, 10);
+  next();
+});
 
-const Usuario = mongoose.model("Usuario",usuarioSchema);
+// Comparar contraseñas
+usuarioSchema.methods.comparePassword = async function (contraseña) {
+  return bcrypt.compare(contraseña, this.contraseña);
+};
+
+const Usuario = mongoose.model("Usuario", usuarioSchema);
 
 module.exports = Usuario;
